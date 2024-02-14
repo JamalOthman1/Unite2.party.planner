b78c3a61cdb867834d969bdc9e90fa8bb584120c
Brittany Dugger <brittanydugger@Brittanys-MBP.attlocal.net>
2024-02-14 13:39:58 -0600

    adding html and javascript files

---
+++ script.js
@@ -0,0 +1,27 @@
+/*
+    SETUP
+ 1. References to different useful things
+ 2. State
+*/
+
+const BASE_URL = 'https://fsa-crud-2aa9294fe819.herokuapp.com/api/brittany-dugger/events'
+
+let events = []
+
+
+
+
+
+
+
+/*
+  EVENT LISTENERS (click submit etc.)
+*/
+
+
+/*
+  RENDER FUNCTIONS
+
+  renderRecipes() - what we call when GET/POST/PUT/DELETE 
+      runs to re-render to the DOM
+*/
